a5e1ac7668b0e6650f6cf3cd130f9dd7f8579aa9
jasonthorne <jasonthorne@live.ie>
2021-10-03 22:51:45 +0100

    assignment2

---
+++ Assignments/Assignment2/js/firebase-config.js
@@ -0,0 +1,18 @@
+
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+	
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+	apiKey: "AIzaSyC72iesVnb1O03nxARvfgc_ZiME94pEXmU",
+	authDomain: "assignment1-b652d.firebaseapp.com",
+	projectId: "assignment1-b652d",
+	storageBucket: "assignment1-b652d.appspot.com",
+	messagingSenderId: "654497800311",
+	appId: "1:654497800311:web:51f52c026f40027bc5a672"
+  };
+	
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
