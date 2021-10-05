d998d1626d29e09165c030c48bccb1915bfeac78
jasonthorne <jasonthorne@live.ie>
2021-10-05 19:32:59 +0100

    Assignment2 firebase shell

--- Assignments/Assignment2/public/js/firebase-config.js
+++ Assignments/Assignment2/public/js/firebase-config.js
@@ -1,18 +1,18 @@
-
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.0.2/firebase-app.js";
+ 
+ // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.1/firebase-app.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
-	
+
   // Your web app's Firebase configuration
   const firebaseConfig = {
-	apiKey: "AIzaSyC72iesVnb1O03nxARvfgc_ZiME94pEXmU",
-	authDomain: "assignment1-b652d.firebaseapp.com",
-	projectId: "assignment1-b652d",
-	storageBucket: "assignment1-b652d.appspot.com",
-	messagingSenderId: "654497800311",
-	appId: "1:654497800311:web:51f52c026f40027bc5a672"
+    apiKey: "AIzaSyCe8vp8DsoVUHjPefErPfm4gcN5DE1M-pU",
+    authDomain: "assignment2-6ea9d.firebaseapp.com",
+    projectId: "assignment2-6ea9d",
+    storageBucket: "assignment2-6ea9d.appspot.com",
+    messagingSenderId: "688069388854",
+    appId: "1:688069388854:web:61a6bc266ff1ad9aa18697"
   };
-	
+
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
