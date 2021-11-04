4ffd070934e2012d1b8072836fa07ff02cd20307
jasonthorne <jasonthorne@live.ie>
2021-11-04 15:01:02 +0000

    js comments file

---
+++ Assignments/Assignment3/public/js/comments.js
@@ -0,0 +1,26 @@
+
+
+var comments = [{make: "Ford", model: "Mondeo", age: 2, mileage: 10000}];
+document.getElementById("testId").value = comments;
+
+
+//load comments from db:
+//function loadComments(){
+function test(){
+
+    //test comments to mimic those pulled remotely:
+    comments.push({make: "Ford", model: "Mondeo", age: 2, mileage: 10000}, 
+    {make: "Ford2", model: "Mondeo2", age: 22, mileage: 100002})
+
+    document.getElementById("testId").value = comments;
+
+
+}
+
+function buildComments(){
+
+}
+
+
+
+
