f130755258474e377f2f3e8685916c55f8819fc2
jasonthorne <jasonthorne@live.ie>
2021-11-04 22:44:23 +0000

    cards test

--- Assignments/Assignment3/public/js/comments.js
+++ Assignments/Assignment3/public/js/comments.js
@@ -1,33 +1,40 @@
 
-
-var comments = []; //holds comments ++++++++++++++++++++++++++comment sucks as a name!! Should be user comment or something :P 
+var comments = []; //holds comments
 
 //load comments from db:
 function loadComments(){
-    //test comments, to mimic those pulled remotely:
-    comments.push( 
-        {handle: "Bob", comment: "I'm Bob", post_date: Date.now()}, 
-        {handle: "Frank", comment: "I b Frank", post_date: Date.now()},
-        {handle: "Billy", comment: "Billy was here", post_date: Date.now()}, 
-        {handle: "Cuthbert", comment: "Cuthbert", post_date: Date.now()}) 
+    comments.push( //add test comments to mimic remote pull:
+        {handle: "@Bob", comment: "Bob comment", post_date: Date.now()}, 
+        {handle: "@Frank", comment: "Frank comment", post_date: Date.now()},
+        {handle: "@Billy", comment: "Billy comment", post_date: Date.now()}, 
+        {handle: "@Terry", comment: "Terry comment", post_date: Date.now()}) 
 
-
-    console.log(comments);
-
-    document.getElementById("testId").innerHTML = comments[1].comment;
-
+    console.log(comments); //+++++++++++
     showComments();
-
-
 }
 
-
+//show comments:
 function showComments(){
+
+    
+
+    var test = 
+    "<table>\n" +
+    "<tr>\n" +
+        "<td>Emil</td>\n" +
+        "<td>Tobias</td>\n" +
+        "<td>Linus</td>\n" +
+    "</tr>\n" +
+    "</table>";
+
+    //document.getElementById("comments").innerHTML = comments[0].post_date;
+
+   /// document.getElementById("comments").innerHTML = test;
 
 }
 
 function addComment(){
 
-}
 
+}
 
