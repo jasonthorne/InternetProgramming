43ffc55da68287de68a60146dba09d45ba90bdaa
jasonthorne <jasonthorne@live.ie>
2021-11-15 21:32:48 +0000

    firebase functions

--- Labs/Week3/DeployingAppToFirebase/functions/index.js
+++ Labs/Week3/DeployingAppToFirebase/functions/index.js
@@ -2,8 +2,19 @@ const functions = require("firebase-functions");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
+
+//+++++++++++++++REMEMBER: These exist on the SERVER side ++++++++++++
+
 //
-	exports.helloWorld = functions.https.onRequest((request, response) => {
-		functions.logger.info("Hello logs!", {structuredData: true});
-		response.send("Hello from Firebase!");
+	exports.helloWorld1 = functions.https.onRequest((request, response) => {
+		///functions.logger.info("Hello logs!", {structuredData: true});
+		//response.send("Sup, sexy!");
+		response.send(request.query.data); //adding 'data' request to be sent to back end (?data= dataToSend)
 	});
+	
+	
+	exports.doubleNumber = functions.https.onRequest((request, response) => {
+		var myDbl = parseInt(request.query.data * 2); //add given '?data= ' from browser, and double it
+		response.send(myDbl); //send back doubled number
+	});
+
