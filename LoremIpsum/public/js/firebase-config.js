d91edfa01229cd169f3363df6632381d4f9ff860
jasonthorne <jasonthorne@live.ie>
2022-03-02 22:49:49 +0000

    new build

---
+++ LoremIpsum/public/js/firebase-config.js
@@ -0,0 +1,18 @@
+ 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.1.3/firebase-app.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+apiKey: "AIzaSyBncdVZQYEiyVp0slQub2kIG5rTwcbQ3pM",
+authDomain: "assignment3-a1b50.firebaseapp.com",
+projectId: "assignment3-a1b50",
+storageBucket: "assignment3-a1b50.appspot.com",
+messagingSenderId: "675718330557",
+appId: "1:675718330557:web:ef984f01c75072f7805fd4"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
