7d0ea71b615c3f84090293f032237863478a2193
jasonthorne <jasonthorne@live.ie>
2022-06-02 00:32:06 +0100

    postcomments test working

--- LoremIpsum/functions/index.js
+++ LoremIpsum/functions/index.js
@@ -1,5 +1,4 @@
 
-
 const functions = require("firebase-functions");
 
 /*admin library for allowing server code (functions) to run in an authenticated mode
@@ -7,6 +6,34 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
+exports.postcomments = functions.https.onRequest((request, response) => {
+
+  //1. receive comment data in here from POST request ".onRequest((request))"
+  //2. connect to firestore db using admin library:
+  //search firestore collections for "comments", then add contents of request body
+  return admin.firestore().collection('comments').add(request.body).then(()=>{
+      //.then() callback function, that's returned from server to client when request.body is added to db:
+      response.send("Saved comment in the database");
+  });
+});
+
+
+
+/*
+exports.postcomment = functions.https.onRequest((request, response) => {
+
+  const currentTime = admin.firestore.Timestamp.now();
+  request.body.timestamp = currentTime;
+
+  return admin.firestore().collection('comments').add(request.body).then(()=>{
+      response.send("Saved comment in the database");
+  });
+});*/
+
+
+
+
+/*
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
@@ -14,3 +41,4 @@ exports.helloWorld = functions.https.onRequest((request, response) => {
    functions.logger.info("Hello logs!", {structuredData: true});
    response.send("Hello dawg!");
  });
+*/
