a9a4eeb790f6b9eabda1c30259d0de22b9c4a160
jasonthorne <jasonthorne@live.ie>
2022-06-02 02:06:53 +0100

    getComments test working

--- LoremIpsum/functions/index.js
+++ LoremIpsum/functions/index.js
@@ -6,17 +6,35 @@ const functions = require("firebase-functions");
 const admin = require("firebase-admin");
 admin.initializeApp();
 
-exports.postcomments = functions.https.onRequest((request, response) => {
+exports.postComment = functions.https.onRequest((request, response) => {
 
-  //1. receive comment data in here from POST request ".onRequest((request))"
-  //2. connect to firestore db using admin library:
-  //search firestore collections for "comments", then add contents of request body
-  return admin.firestore().collection('comments').add(request.body).then(()=>{
-      //.then() callback function, that's returned from server to client when request.body is added to db:
-      response.send("Saved comment in the database");
+//1. receive comment data in here from POST request ".onRequest((request))"
+//2. connect to firestore db using admin library:
+//search firestore collections for "comments", then add contents of request body
+return admin.firestore().collection("comments").add(request.body).then(()=>{
+    //.then() callback function, that's returned from server to client when request.body is added to db:
+    response.send("Saved comment in the database");
   });
 });
 
+exports.getComments = functions.https.onRequest((request, response) => {
+
+  // 1. Connect to our Firestore database
+  let myData = []
+  //////////admin.firestore().collection("comments").orderBy("date", "desc").get().then((snapshot) => {
+  admin.firestore().collection("comments").get().then((snapshot) => {
+      if (snapshot.empty) {
+          console.log("No matching documents.");
+          response.send("No data in database");
+          return; //return if no data
+      }
+      snapshot.forEach(doc => { 
+        myData.push(doc.data()); //add raw object data
+      });
+      // 2. Send data back to client
+      response.send(myData);
+  })
+});
 
 
 /*
