cede77a4b3cc9e745624164019868340bdcd7410
jasonthorne <jasonthorne@live.ie>
2022-06-08 22:43:36 +0100

    modal styles complete (for now)

--- LoremIpsum/public/js/comments.js
+++ LoremIpsum/public/js/comments.js
@@ -16,8 +16,8 @@ var commentTooltip = makeToolTip("comment-input", "Enter Comment");  //comment t
 //get comments from db:
 function getComments(){
 
-    $("#comment-modal").modal("show");
-    //$("#delete-comment-modal").modal("show"); 
+    //$("#comment-modal").modal("show");
+    $("#delete-comment-modal").modal("show"); 
     
     /*let request = new XMLHttpRequest(); //request object
     //open get request on firebase's 'getComments' function:
