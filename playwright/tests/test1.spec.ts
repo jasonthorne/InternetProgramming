cb7a767a7eb5b52595da8d4ebaf48ac6dcf36632
jasonthorne <jasonthorne@live.ie>
2024-04-08 17:18:32 +0100

    notes

--- playwright/tests/test1.spec.ts
+++ playwright/tests/test1.spec.ts
@@ -7,7 +7,7 @@ let homePage: HomePage;
 //const URL = 'https://lorem-ipsum-fc.web.app/'; //++++++++++++TEST
 
 test.beforeEach(async({page})=>{
-    await page.goto(pageData.url);
+    await page.goto(pageData.url); //////IS HOMEPAGE NEEDED HERE - Could it just be 'Page'??????
     homePage = new HomePage(page, /Lorem Ipsum F.C./); //++cHange this ot just pass oage, and have class pull title in itself.
 });
 
