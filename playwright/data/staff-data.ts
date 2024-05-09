e11e093ed2a89c9bc7ebc59ce7632bc11a4af434
jasonthorne <jasonthorne@live.ie>
2024-05-09 17:09:06 +0100

    cleanup staff data

--- playwright/data/staff-data.ts
+++ playwright/data/staff-data.ts
@@ -1,35 +1,41 @@
+class Staff{
+    constructor(
+        readonly title: string,
+        readonly name: string,
+        readonly img: Img,
+        readonly text: string
+    ){}
+}
+
+class Img{
+    constructor(
+        readonly src: string, 
+        readonly alt: string
+    ){}
+}
 
 export const staffData = {
-    chairperson: {
-        title: 'Chairperson',
-        name: 'John Doe',
-        img: {
-            src: 'img/staff/chairperson.png',
-            alt: 'chairperson'
-        },
-        text: `Integer et turpis ante. Nulla sed libero turpis. 
+    chairperson: new Staff(
+        'Chairperson',
+        'John Doe',
+        new Img('img/staff/chairperson.png', 'chairperson'),
+        `Integer et turpis ante. Nulla sed libero turpis. 
         Aenean vel ligula laoreet, mollis odio sit amet, aliquet quam. 
         Nam sed venenatis diam, pulvinar consectetur lacus.`
-    },
-    secretary: {
-        title: 'Secretary',
-        name: 'John Roe',
-        img: {
-            src: 'img/staff/secretary.png',
-            alt: 'secretary'
-        },
-        text: `Etiam facilisis augue eget aliquet accumsan. Proin at nisl massa. 
+    ),
+    secretary: new Staff(
+        'Secretary',
+        'John Roe',
+        new Img('img/staff/secretary.png', 'secretary'),
+        `Etiam facilisis augue eget aliquet accumsan. Proin at nisl massa. 
         Aliquam a lorem congue, ornare ligula quis, dictum enim.`
-    },
-    treasurer: {
-        title: 'Treasurer',
-        name: 'Jane Doe',
-        img: {
-            src: 'img/staff/treasurer.png',
-            alt: 'treasurer'
-        },
-        text: `Proin venenatis lorem at mauris eleifend aliquam. 
+    ),
+    treasurer: new Staff(
+        'Treasurer',
+        'Jane Doe',
+        new Img('img/staff/treasurer.png', 'treasurer'),
+        `Proin venenatis lorem at mauris eleifend aliquam. 
         Orci varius natoque et magnis dis parturient montes, nascetur ridiculus mus. 
         Praesent eu rhoncus leo, et commodo nibh.`
-    }
+    )
  } as const;
