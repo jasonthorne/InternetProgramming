4975954133272f9ea2cdbbd47ade0fc0386a30dc
jasonthorne <jasonthorne@live.ie>
2024-05-10 01:19:57 +0100

    updating footer data

--- playwright/data/footer-data.ts
+++ playwright/data/footer-data.ts
@@ -1,4 +1,15 @@
 
+class Link{
+    constructor(
+        readonly href: string,
+        readonly text: string
+    ){}
+}
+
+class Host{
+    constructor(readonly link: Link){}
+}
+
 export const footerData = {
     creator: {
         text: 'Made by Jason Thorne. 2022',
@@ -9,35 +20,35 @@ export const footerData = {
     },
     pitch: {
         text: 'Pitch Image by Rudy and Peter Skitterians from Pixabay',
-        link: {
-            href: 'https://pixabay.com/users/skitterphoto-324082/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1269438',
-            text: 'Rudy and Peter Skitterians'
-        },
-        host: {
-            link: {
-                href: 'https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1269438',
-                text: 'Pixabay'
-            }
-        }
+        link: new Link(
+            'https://pixabay.com/users/skitterphoto-324082/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1269438',
+            'Rudy and Peter Skitterians'
+        ),
+        host: new Host(
+            new Link(
+               'https://pixabay.com/?utm_source=link-attribution&amp;utm_medium=referral&amp;utm_campaign=image&amp;utm_content=1269438',
+               'Pixabay'
+            )
+        )
     },
     team_and_staff: {
         text: 'Team and Staff Icons made by Freepik from www.flaticon.com',
-        link: {
-            href: 'https://www.freepik.com',
-            text: 'Freepik'
-        },
-        host: {
-            link:{
-                href: 'https://www.flaticon.com',
-                text: 'www.flaticon.com'
-            }
-        }
+        link: new Link(
+            'https://www.freepik.com',
+            'Freepik'
+        ),
+        host: new Host(
+            new Link(
+                'https://www.flaticon.com',
+               'www.flaticon.com'
+            )
+        )
     },
     footer_and_comment: {
         text: 'Footer and Comment Icons from Font Awesome',
-        link: {
-            href: 'https://fontawesome.com',
-            text: 'Font Awesome'
-        }
+        link: new Link(
+            'https://fontawesome.com',
+            'Font Awesome'
+        )
     }
 } as const;
