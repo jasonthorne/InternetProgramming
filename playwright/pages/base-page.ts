4aea012ceeaf0df6ce9fa43121a06f779b679572
jasonthorne <jasonthorne@live.ie>
2024-05-14 00:19:49 +0100

    cleanup

--- playwright/pages/base-page.ts
+++ playwright/pages/base-page.ts
@@ -5,77 +5,129 @@ export class BasePage{
     constructor(readonly page: Page){}
 
     //assert page title:
-    async assertPageTitle(title: string){ 
-        await expect(this.page).toHaveTitle(title);
+    async assertPageTitle(title: string){
+        try{
+            await expect(this.page).toHaveTitle(title);
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert image is visible:
     async assertImgIsVisible(selector: string, alt: string){ 
-        await expect(this.page.locator(selector)
-            .getByRole('img',{name: alt})).toBeVisible();
+        try{
+            await expect(this.page.locator(selector)
+                .getByRole('img',{name: alt})).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert image src:
     async assertImgSrc(selector: string, alt: string, src: string){
-        expect(await this.page.locator(selector).getByRole('img',{name: alt})
-            .getAttribute('src')).toMatch(src);
+        try{
+            expect(await this.page.locator(selector).getByRole('img',{name: alt})
+                .getAttribute('src')).toMatch(src);
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert text is visible:
     async assertTextIsVisible(selector: string, text: string){
-        await expect(this.page.locator(selector).getByText(text)).toBeVisible();
+        try{
+            await expect(this.page.locator(selector).getByText(text)).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
      //assert link href:
     async assertLinkHref(selector: string, name: string, href: string){
-        expect(await this.page.locator(selector).getByRole('link',{name:name})
-        .getAttribute('href')).toMatch(href);
+        try{
+            expect(await this.page.locator(selector).getByRole('link',{name:name})
+            .getAttribute('href')).toMatch(href);
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert element is visible:
     async assertElementIsVisible(selector: string){
-        await expect(this.page.locator(selector)).toBeVisible();
+        try{
+            await expect(this.page.locator(selector)).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert element is hidden:
     async assertElementIsHidden(selector: string){
-        await expect(this.page.locator(selector)).toBeHidden();
+        try{
+            await expect(this.page.locator(selector)).toBeHidden();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert background image:
     async assertBackgroundImg(selector: string, src: string){
-        const imgSrc: string = await this.page.locator(selector).evaluate((element: HTMLElement)=>{
-            return window.getComputedStyle(element).getPropertyValue('background-image');
-        });
-        expect(imgSrc).toContain(src);
+        try{
+            const imgSrc: string = await this.page.locator(selector).evaluate((element: HTMLElement)=>{
+                return window.getComputedStyle(element).getPropertyValue('background-image');
+            });
+            expect(imgSrc).toContain(src);
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert button is visible:
     async assertBtnIsVisible(name: string){
-        await expect(this.page.getByRole('button',{name: name})).toBeVisible();
+        try{
+            await expect(this.page.getByRole('button',{name: name})).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
   
     //enter text into input field:
     async enterInputFieldText(selector: string, text: string){
-        await this.page.locator(selector).fill(text);
+        try{
+            await this.page.locator(selector).fill(text);
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //click target buton:
     async clickButton(selector: string, text: string){
-        await this.page.locator(selector).getByRole('button',{name:text}).click();
+        try{
+            await this.page.locator(selector).getByRole('button',{name:text}).click();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert tooltip is visible:
     async assertTooltipIsVisible(text: string){
-        await expect(this.page.getByRole('tooltip',{name:text})).toBeVisible();
+        try{
+            await expect(this.page.getByRole('tooltip',{name:text})).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert that text is hiiden on given width
     async assertTextIsHiddenOnViewportWidth(selector: string, text: string, width: number){
-        const viewportSize: {width:number; height:number} = this.page.viewportSize();
-        await this.page.setViewportSize({width:width, height:viewportSize.height});
-        await expect(this.page.locator(selector).getByText(text)).toBeHidden();
-        await this.page.setViewportSize({width:viewportSize.width, height:viewportSize.height});
+        try{
+            const viewportSize: {width:number; height:number} = this.page.viewportSize();
+            await this.page.setViewportSize({width:width, height:viewportSize.height});
+            await expect(this.page.locator(selector).getByText(text)).toBeHidden();
+            await this.page.setViewportSize({width:viewportSize.width, height:viewportSize.height});
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 }
 
--- playwright/pages/index-page.ts
+++ playwright/pages/index-page.ts
@@ -9,16 +9,24 @@ export class IndexPage extends BasePage{
 
     //assert navbar link is valid:
     async assertNavbarLink(navbarClass: string, name: string, url: string){
-        await this.page.locator(navbarClass).getByRole('link',{name:name}).click();
-        await expect(this.page).toHaveURL(new RegExp(`/${url}$`));
+        try{
+            await this.page.locator(navbarClass).getByRole('link',{name:name}).click();
+            await expect(this.page).toHaveURL(new RegExp(`/${url}$`));
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert 'news & updates' content is visible:
     async assertNewsUpdatesContentIsVisible(sectionClass: string, heading: string, text: string){
-        await expect(this.page.locator(sectionClass)
-            .filter({hasText: heading})
-            .filter({hasText: text})
-        ).toBeVisible();
+        try{
+            await expect(this.page.locator(sectionClass)
+                .filter({hasText: heading})
+                .filter({hasText: text})
+            ).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert fixture result content is visible:
@@ -26,6 +34,7 @@ export class IndexPage extends BasePage{
         resultsClass: string, 
         homeTeam: {logo:string; name:string; score:string},
         awayTeam: {logo:string; name:string; score:string}){
+        try{
             await expect(this.page.locator(resultsClass)
                 .filter({has: this.page.getByRole('img',{name: homeTeam.logo})})
                 .filter({hasText: homeTeam.name})
@@ -34,24 +43,35 @@ export class IndexPage extends BasePage{
                 .filter({hasText: awayTeam.name})
                 .filter({hasText: awayTeam.score})
             ).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert admin content is visible:
     async assertAdminContentIsVisible(
         sectionClass: string,
         admin: {title:string; img:string; name:string, text:string}){
+        try{
             await expect(this.page.locator(sectionClass)
                 .filter({hasText: admin.title})
                 .filter({has: this.page.getByRole('img',{name: admin.img})})
                 .filter({hasText: admin.name})
                 .filter({hasText: admin.text})
             ).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert input field:
     async assertInputField(formId: string, placeholder: string, maxlength: string){
-        await expect(this.page.locator(formId)).toHaveAttribute('placeholder', placeholder);
-        await expect(this.page.locator(formId)).toHaveAttribute('maxlength', maxlength);
+        try{
+            await expect(this.page.locator(formId)).toHaveAttribute('placeholder', placeholder);
+            await expect(this.page.locator(formId)).toHaveAttribute('maxlength', maxlength);
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert footer creator link is valid:
@@ -70,17 +90,22 @@ export class IndexPage extends BasePage{
     //aseert coment form is visible:
     async assertCommentFormIsVisible(
         sectionClass: string, form: {handle:string; comment:string; button:string}){
+        try{
             await expect(this.page.locator(sectionClass)
                 .filter({has: this.page.getByPlaceholder(form.handle)})
                 .filter({has: this.page.getByPlaceholder(form.comment)})
                 .filter({has: this.page.getByRole('button',{name: form.button})})
             ).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert comment modal is visible:
     async assertCommentModalIsVisible(
         modal: {id:string; header:string; submitBtn:string; cancelBtn:string},
         commentData: {handle:string, comment:string}){
+        try{
             await expect(this.page.locator(modal.id)
                 .filter({hasText: modal.header})
                 .filter({hasText: commentData.handle})
@@ -88,82 +113,85 @@ export class IndexPage extends BasePage{
                 .filter({has: this.page.getByRole('button',{name: modal.submitBtn})})
                 .filter({has: this.page.getByRole('button',{name: modal.cancelBtn})})
             ).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert comment is visible:
     async assertCommentIsVisible(
         comment: {class:string; likesClass:string; handle:string; 
         comment:string; date:string; likeBtn:string; deleteBtn:string}){
-            try{
-                await expect(this.page.locator(comment.class)
-                    .filter({hasText: await this.page.locator(comment.likesClass).innerText()})
-                    .filter({hasText: comment.handle})
-                    .filter({hasText: comment.comment})
-                    .filter({hasText: comment.date})
-                    .filter({has: this.page.getByRole('button',{name:comment.likeBtn})})
-                    .filter({has: this.page.getByRole('button',{name:comment.deleteBtn})})
-                ).toBeVisible();
-            }catch(error){
-                console.error(`An error occurred: ${error.message}`);
-            }
+        try{
+            await expect(this.page.locator(comment.class)
+                .filter({hasText: await this.page.locator(comment.likesClass).innerText()})
+                .filter({hasText: comment.handle})
+                .filter({hasText: comment.comment})
+                .filter({hasText: comment.date})
+                .filter({has: this.page.getByRole('button',{name:comment.likeBtn})})
+                .filter({has: this.page.getByRole('button',{name:comment.deleteBtn})})
+            ).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert like button clicks:
     async assertLikeBtnClicks(
         comment: {class:string; date:string; likesClass:string;
         likeBtnClass:string; unlikedClass:string; likedClass:string}){
-            try{
-                //grab locators from comment of given date:
-                const commentLocator: Locator = this.page.locator(comment.class).filter({hasText: comment.date});
-                const likeBtnLocator: Locator = commentLocator.locator(comment.likeBtnClass);
-                const unlikedIconLocator: Locator = likeBtnLocator.locator(comment.unlikedClass);
-                const likedIconLocator: Locator = likeBtnLocator.locator(comment.likedClass);
-                const likesLocator: Locator = commentLocator.locator(comment.likesClass);
-                let likes: number = parseInt((await likesLocator.innerText()).trim()); //grab likes value
+        try{
+            //grab locators from comment of given date:
+            const commentLocator: Locator = this.page.locator(comment.class).filter({hasText: comment.date});
+            const likeBtnLocator: Locator = commentLocator.locator(comment.likeBtnClass);
+            const unlikedIconLocator: Locator = likeBtnLocator.locator(comment.unlikedClass);
+            const likedIconLocator: Locator = likeBtnLocator.locator(comment.likedClass);
+            const likesLocator: Locator = commentLocator.locator(comment.likesClass);
+            let likes: number = parseInt((await likesLocator.innerText()).trim()); //grab likes value
 
-                await expect(commentLocator).toBeVisible(); //check comment is visible
-                await expect(unlikedIconLocator).toBeVisible(); //confirm unliked icon is visible
-                await likeBtnLocator.click(); //click like btn (to like)
-                expect(parseInt((await likesLocator.innerText()).trim())).toBe(++likes); //confirm likes increase
-                await expect(likedIconLocator).toBeVisible(); //confirm liked icon is visible
-                await likeBtnLocator.click(); //click like btn again (to unlike)
-                expect(parseInt((await likesLocator.innerText()).trim())).toBe(--likes); //confirm likes decrease
-                await expect(unlikedIconLocator).toBeVisible(); //confirm unliked icon is visible
-            }catch(error){
-                console.error(`An error occurred: ${error.message}`);
-            }
+            await expect(commentLocator).toBeVisible(); //check comment is visible
+            await expect(unlikedIconLocator).toBeVisible(); //confirm unliked icon is visible
+            await likeBtnLocator.click(); //click like btn (to like)
+            expect(parseInt((await likesLocator.innerText()).trim())).toBe(++likes); //confirm likes increase
+            await expect(likedIconLocator).toBeVisible(); //confirm liked icon is visible
+            await likeBtnLocator.click(); //click like btn again (to unlike)
+            expect(parseInt((await likesLocator.innerText()).trim())).toBe(--likes); //confirm likes decrease
+            await expect(unlikedIconLocator).toBeVisible(); //confirm unliked icon is visible
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert delete comment modal is visible:
     async assertDeleteCommentModalIsVisible(
         modal: {id:string; header:string; text:string; deleteBtn:string; cancelBtn:string}){
-            try{
-                await expect(this.page.locator(modal.id)
-                    .filter({hasText: modal.header})
-                    .filter({hasText: modal.text})
-                    .filter({has: this.page.getByRole('button',{name: modal.deleteBtn})})
-                    .filter({has: this.page.getByRole('button',{name: modal.cancelBtn})})
-                ).toBeVisible();
-            }catch(error){
-                console.error(`An error occurred: ${error.message}`);
-            }
+        try{
+            await expect(this.page.locator(modal.id)
+                .filter({hasText: modal.header})
+                .filter({hasText: modal.text})
+                .filter({has: this.page.getByRole('button',{name: modal.deleteBtn})})
+                .filter({has: this.page.getByRole('button',{name: modal.cancelBtn})})
+            ).toBeVisible();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert comment is hidden:
     async assertCommentIsHidden(
         comment: {class:string; handle:string; comment:string; 
         date:string; likeBtn:string; deleteBtn:string}){
-            try{
-                await expect(this.page.locator(comment.class)
-                    .filter({hasText: comment.handle})
-                    .filter({hasText: comment.comment})
-                    .filter({hasText: comment.date})
-                    .filter({has: this.page.getByRole('button',{name: comment.likeBtn})})
-                    .filter({has: this.page.getByRole('button',{name: comment.deleteBtn})})
-                ).toBeHidden();
-            }catch(error){
-                console.error(`An error occurred: ${error.message}`);
-            }
+        try{
+            await expect(this.page.locator(comment.class)
+                .filter({hasText: comment.handle})
+                .filter({hasText: comment.comment})
+                .filter({hasText: comment.date})
+                .filter({has: this.page.getByRole('button',{name: comment.likeBtn})})
+                .filter({has: this.page.getByRole('button',{name: comment.deleteBtn})})
+            ).toBeHidden();
+        }catch(error){
+            console.error(`An error occurred: ${error.message}`);
+        }
     }
 
     //assert footer attribute link is valid:
